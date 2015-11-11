60d6143864a2e89e47373e15b468eabee3eb1941
angasch <anne.gabriele.schroeder@uni-duesseldorf.de>
2015-11-11 17:50:19 +0100

    Update deriv.cxx

--- deriv.cxx
+++ deriv.cxx
@@ -11,6 +11,26 @@ void print(double* p, const int N, const double dx,
        cout << i*dx + xmin << "\t" << p[i] << endl;
 }
 
+void func(double* p, const int N, const double dx, const double xmin){
+  double x;
+  for(int i=0; i<N; i++){
+    x = i*dx + xmin;
+    p[i] = exp(-x*x);
+  }
+    
+}
+
+void deriv(double* p, const int N, const double dx){
+ double a;
+ double b=0;
+  for(int i=0; i<N-1;i++){
+    a=p[i];
+    p[i] = (p[i+1]- b)/(2*dx);
+    b=a;
+  }
+  p[N-1] = (0 - b)/(2*dx);
+}
+
 
 int main(){
   const int N = 256;
@@ -19,8 +39,12 @@ int main(){
   const double xmin = -15;
   const double dx = (xmax-xmin)/(N-1);
 
-  // call to function which fills array p here
-  // call to functio which calculates the derivative
+  func(p,N,dx,xmin);  // call to function which fills array p here
+  
+  
+  deriv(p,N,dx); // call to function which calculates the derivative
+  
+  
   print(p,N,dx,xmin);
 
   return 0;
